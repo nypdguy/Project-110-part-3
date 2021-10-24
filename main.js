647c7151eac14cd7b875b0230569eafa7aa03ce3
tvcdds <88736561+nypdguy@users.noreply.github.com>
2021-10-24 16:21:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera')
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xQRttb4CE/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!')
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifer.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+    }
+        if(results[0].label == "amazing"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "best"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+    }
